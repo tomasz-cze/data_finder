4423124e9bf0e22a79ab967545f7b8e97415fda3
unknown <tomasz.czechlewski@gmail.com>
2022-02-02 14:27:12 +0100

    main function and listener added. Need to make something with big letteres

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const input = document.querySelector('.search')
+const dataList = document.querySelector('ul')
+const datas = document.querySelectorAll('li')
 
+const search = () => {
+	const searchData = input.value
+
+	for (let i = 0; i < datas.length; i++) {
+        if (datas[i].textContent.includes(searchData)) {
+        datas[i].classList.remove('hidden')
+        } else {
+            datas[i].classList.add('hidden')
+        }
+	}
+}
+
+
+input.addEventListener('keyup', search)
